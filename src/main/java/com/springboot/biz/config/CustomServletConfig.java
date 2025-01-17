65857ee4e80e8e28c2f5d6631b93ea1e85f9df87
joshbae119 <mclceo119@gmail.com>
2025-01-17 12:52:46 +0900

    cors 설정완료

---
+++ src/main/java/com/springboot/biz/config/CustomServletConfig.java
@@ -0,0 +1,26 @@
+package com.springboot.biz.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.springboot.biz.controller.formatter.LocalDateFormatter;
+
+@Configuration
+public class CustomServletConfig implements WebMvcConfigurer {
+	
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addFormatter(new LocalDateFormatter());
+	}
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/**")
+		.allowedOrigins("*")
+		.allowedMethods("HEAD","GET","POST","PUT","DELETE","OPTIONS")
+		.maxAge(300)
+		.allowedHeaders("Authotization","Cache-Control","Content-Type");
+	}
+
+}
--- src/main/java/com/springboot/biz/controller/TodoController.java
+++ src/main/java/com/springboot/biz/controller/TodoController.java
@@ -1,7 +1,12 @@
 package com.springboot.biz.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,8 @@ import com.springboot.biz.dto.TodoDto;
 import com.springboot.biz.service.TodoService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +35,29 @@ public class TodoController {
 	public PageResponseDto<TodoDto> list(PageRequestDto pageRequestDto){
 		return todoService.list(pageRequestDto);
 	}
+	
+	@PostMapping("/")
+	public Map<String, Long> register(@RequestBody TodoDto todoDto){
+		Long tno = todoService.register(todoDto);
+		return Map.of("TNO", tno);
+	}
+	
+	@PutMapping("/{tno}")
+	public Map<String, String> modify(@PathVariable(name="tno") Long tno, @RequestBody TodoDto todoDto) {
+		todoDto.setTno(tno);
+		
+		todoService.modify(todoDto);
+		return Map.of("RESULT", "성공");
+	}
+	
+	@DeleteMapping("/{tno}")
+	public Map<String, String> remove(@PathVariable(name= "tno") Long tno){
+		todoService.remove(tno);
+		return Map.of("결과", "성공");
+	}
+	
+	
+	
+	
 
 }
