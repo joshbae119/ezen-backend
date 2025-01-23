635217e3f0973986126816066a21bcb340209097
joshbae119 <mclceo119@gmail.com>
2025-01-23 13:42:51 +0900

    2025.1.23 점심시간

--- src/test/java/com/springboot/biz/MemberRepositoryTest.java
+++ src/test/java/com/springboot/biz/MemberRepositoryTest.java
@@ -21,25 +21,32 @@ public class MemberRepositoryTest {
 	@Test
 	public void testInserManager() {
 		
-		for( int i=0; i<10 ; i++) {
-			Member member = Member.builder()
-					.email("user" + i + "@aaa.com")
-					.pw(passwordEncoder.encode("1111"))
-					.nickname("USER" + i )
-					.build();
-			member.addRole(MemberRole.USER);
-			
-			if(i >= 5) {
-				member.addRole(MemberRole.MANAGER);
-			}
-			if(i >= 8) {
-				member.addRole(MemberRole.ADMIN);
-			}
-			
-			memberRepository.save(member);
-		}
-
+//		for( int i=0; i<10 ; i++) {
+//			Member member = Member.builder()
+//					.email("user" + i + "@aaa.com")
+//					.pw(passwordEncoder.encode("1111"))
+//					.nickname("USER" + i )
+//					.build();
+//			member.addRole(MemberRole.USER);
+//			
+//			if(i >= 5) {
+//				member.addRole(MemberRole.MANAGER);
+//			}
+//			if(i >= 8) {
+//				member.addRole(MemberRole.ADMIN);
+//			}
+//			
+//			memberRepository.save(member);
+//		}
 		
 	}
+	@Test
+    public void testRead() {
+	 
+		String email = "user0@aaa.com";
+	    Member member = memberRepository.getWithRoles(email);
+	    System.out.println("내가 검색한 객체 : " + member);
+	}
+
 
 }
