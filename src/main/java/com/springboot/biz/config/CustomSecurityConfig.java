4d2d709e2e5c1c9abe173325193102f9f0d7479d
joshbae119 <mclceo119@gmail.com>
2025-01-23 10:06:34 +0900

    2025.1.23 JWT 수업시작

---
+++ src/main/java/com/springboot/biz/config/CustomSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.springboot.biz.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Configuration
+@Log4j2
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+}
